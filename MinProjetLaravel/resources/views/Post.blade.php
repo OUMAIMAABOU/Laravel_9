4fec9d8315ce511ea4e50667d2c9c4df63ceb250
oumaima <oumaimaabouelhaitam@gmail.com>
2022-07-05 15:55:00 +0100

    add form post =>html

---
+++ MinProjetLaravel/resources/views/Post.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post" id="step1">
+        <h1>1</h1>
+        <div>
+            <label for="title">Title</label>
+            <input type="text" name ="title" id="title">
+        </div>
+        <div>
+            <label for="Slug">Slug</label>
+            <input type="text" name ="slug" id="slug">
+        </div>
+        <div>
+            <label for="title">Categorie</label>
+            <select id="Categorie" name="Categorie">
+              <option value="valeur3">Valeur 3</option>
+            </select>
+        </div>
+        <div>
+            <label for="title">tag</label>
+            <select id="tag" name="tag">
+              <option value="valeur3">Valeur 3</option>
+            </select>
+        </div>
+
+    </form>
+</body>
+</html>
